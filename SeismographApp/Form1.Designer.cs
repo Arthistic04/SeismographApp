9f0bb37212f71f1eac919effc70fdf4042f3fb92
Arthistic04 <arthurgulane@gmail.com>
2023-10-27 20:17:56 +0800

    Initial commit

---
+++ SeismographApp/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SeismographApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.labelResult1 = new System.Windows.Forms.Label();
+            this.labelResult2 = new System.Windows.Forms.Label();
+            this.labelResult3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.DimGray;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Emboss;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(2, 3);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(1041, 448);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // labelResult1
+            // 
+            this.labelResult1.AutoSize = true;
+            this.labelResult1.Location = new System.Drawing.Point(1049, 9);
+            this.labelResult1.Name = "labelResult1";
+            this.labelResult1.Size = new System.Drawing.Size(35, 13);
+            this.labelResult1.TabIndex = 4;
+            this.labelResult1.Text = "label1";
+            // 
+            // labelResult2
+            // 
+            this.labelResult2.AutoSize = true;
+            this.labelResult2.BackColor = System.Drawing.Color.Red;
+            this.labelResult2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResult2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult2.Location = new System.Drawing.Point(1052, 22);
+            this.labelResult2.Name = "labelResult2";
+            this.labelResult2.Size = new System.Drawing.Size(117, 41);
+            this.labelResult2.TabIndex = 5;
+            this.labelResult2.Text = "label1";
+            // 
+            // labelResult3
+            // 
+            this.labelResult3.AutoSize = true;
+            this.labelResult3.BackColor = System.Drawing.Color.GreenYellow;
+            this.labelResult3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResult3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult3.Location = new System.Drawing.Point(1052, 22);
+            this.labelResult3.Name = "labelResult3";
+            this.labelResult3.Size = new System.Drawing.Size(117, 41);
+            this.labelResult3.TabIndex = 6;
+            this.labelResult3.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 450);
+            this.Controls.Add(this.labelResult2);
+            this.Controls.Add(this.labelResult1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.labelResult3);
+            this.MaximumSize = new System.Drawing.Size(1252, 489);
+            this.MinimumSize = new System.Drawing.Size(1252, 489);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label labelResult1;
+        private System.Windows.Forms.Label labelResult2;
+        private System.Windows.Forms.Label labelResult3;
+    }
+}
+
---
+++ SeismographApp/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO.Ports;
+using System.Management;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SeismographApp
+{
+    public partial class MainForm : Form
+    {
+        private SerialPort serialPort;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeSerialPort();
+            this.FormClosing += MainForm_FormClosing;
+
+            // Set up chart properties
+            //chart1.ChartAreas[0].AxisX.Minimum = 0;
+            //chart1.ChartAreas[0].AxisX.Maximum = 500; // Adjust as needed
+            chart1.ChartAreas[0].AxisY.Minimum = -20;
+            chart1.ChartAreas[0].AxisY.Maximum = 20; // Adjust as needed
+
+            chart1.ChartAreas[0].AxisX.IsStartedFromZero = false;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "HH:mm:ss"; // Optional: Format the X-axis label as desired
+
+            // Add X-Axis series
+            chart1.Series.Add("X-Axis");
+            chart1.Series["X-Axis"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["X-Axis"].XValueType = ChartValueType.DateTime;
+
+            // Add X-Axis series
+            //chart1.Series.Add("X-Axis");
+            chart1.Series["X-Axis"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            // Add Y-Axis series
+            chart1.Series.Add("Y-Axis");
+            chart1.Series["Y-Axis"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            // Add Z-Axis series
+            chart1.Series.Add("Z-Axis");
+            chart1.Series["Z-Axis"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+        }
+
+        
+        private void InitializeSerialPort()
+        {
+            serialPort = new SerialPort("COM3", 115200);
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            string[] values = data.Split(',');
+
+            if (values.Length >= 6 &&
+                float.TryParse(values[0], out float x) && float.TryParse(values[1], out float y) && float.TryParse(values[2], out float z) &&
+                float.TryParse(values[3], out float x1) && float.TryParse(values[4], out float y1) && float.TryParse(values[5], out float z1))
+            {
+                if (this.IsHandleCreated)
+                {
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        // Get the current timestamp
+                        DateTime timestamp = DateTime.Now;
+
+                        // Add data points with the timestamp as X-value
+                        chart1.Series["X-Axis"].Points.AddXY(timestamp, x);
+                        chart1.Series["Y-Axis"].Points.AddXY(timestamp, y);
+                        chart1.Series["Z-Axis"].Points.AddXY(timestamp, z);
+
+                        DateTime oneMinuteAgo = DateTime.Now.AddSeconds(-30);
+                        DateTime oneMinuteAhead = DateTime.Now.AddSeconds(30);
+                        chart1.ChartAreas[0].AxisX.Minimum = oneMinuteAgo.ToOADate();
+                        chart1.ChartAreas[0].AxisX.Maximum = oneMinuteAhead.ToOADate();
+
+                        // Remove data points older than a specific time window (e.g., 10 minutes)
+                        while (chart1.Series["X-Axis"].Points[0].XValue < oneMinuteAgo.ToOADate())
+                        {
+                            chart1.Series["X-Axis"].Points.RemoveAt(0);
+                            chart1.Series["Y-Axis"].Points.RemoveAt(0);
+                            chart1.Series["Z-Axis"].Points.RemoveAt(0);
+                        }
+
+                        double accelerationMagnitude = CalculateEquation(x1, y1, z1);
+                        labelResult1.Text = $"Result: {accelerationMagnitude}";
+
+                    });
+                }
+            }
+        }
+
+        private double CalculateEquation(float x, float y, float z)
+        {
+            double accelerationMagnitude = Math.Sqrt(x * x + y * y + z * z);
+            if (accelerationMagnitude > 10.0)
+            {
+                labelResult1.Text = $"Result: {accelerationMagnitude}";
+                labelResult2.Text = "Earthquake\nDetected";
+                labelResult3.Text = "";
+            }
+            else
+            {
+                labelResult2.Text = "";
+                labelResult3.Text = "                 \n        ";
+            }
+            return accelerationMagnitude;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+    }
+}
